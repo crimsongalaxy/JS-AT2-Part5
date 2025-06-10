aee016a1196002d4f2df1e007b09d2cb2164b282
crimsongalaxy <rhodgskiss@student.holmesglen.edu.au>
2025-06-11 08:35:59 +1000

    app.js planning.
    Added the planning for what will happen in this file.

--- website/scripts/app.js
+++ website/scripts/app.js
@@ -1,1 +1,26 @@
-// 
+// Create an array of movies
+
+// Create an instance of the MovieList class
+// MovieList takes in the rootId and the movie array
+
+// Event functions
+// Search click
+// A - Z click
+// Z - A click
+
+// Add a new movie to the list
+
+// Update a movie in the list
+
+// Delete a movie from the list
+
+// UI Javascript
+// Tabs section
+// Function openForm
+// Take in 2 parameters, and event and an action
+// Return nothing
+
+// Open tab by default
+
+// Footer
+// Inject the date into the footer
